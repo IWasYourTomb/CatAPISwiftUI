8bac05f07899dc61792ce869905d054ee15b005b
IWasYourTomb <67316822+IWasYourTomb@users.noreply.github.com>
2022-11-25 22:40:02 +0300

    Initial Commit

---
+++ catAPI/catAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  catAPIApp.swift
+//  catAPI
+//
+//  Created by matvey on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct catAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
